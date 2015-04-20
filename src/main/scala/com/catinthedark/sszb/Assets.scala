124765a7ab0fb1c469d1d78a6811b7eb8153b0a2
over <overln2@gmail.com>
2015-04-21 00:24:25 +0600

    New screen for menu / gameover

--- src/main/scala/com/catinthedark/sszb/Assets.scala
+++ src/main/scala/com/catinthedark/sszb/Assets.scala
@@ -15,10 +15,10 @@ object Assets {
   object Textures {
     val logo = new Texture(Gdx.files.internal("textures/logo.png"))
 
-    val t1 = new Texture(Gdx.files.internal("textures/menu.png"))
+    val t1 = new Texture(Gdx.files.internal("textures/menu.gif"))
     val t2 = new Texture(Gdx.files.internal("textures/t1.png"))
     val t3 = new Texture(Gdx.files.internal("textures/t2.png"))
-    val gameOver = new Texture(Gdx.files.internal("textures/gameover.png"))
+    val gameOver = new Texture(Gdx.files.internal("textures/gameover.gif"))
     val gameWin = new Texture(Gdx.files.internal("textures/gamewin.png"))
     val hudBack = new Texture(Gdx.files.internal("textures/hud_back.png"))
     val hud = new Texture(Gdx.files.internal("textures/hud.png"))
